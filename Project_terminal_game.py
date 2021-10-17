5578b95c93fbe31a72ee250546848577338f2e19
ElianeSato <eliane.sato.dev@gmail.com>
2021-10-17 10:37:02 -0300

    Implements subclass Encargo_mensal

--- Project_terminal_game.py
+++ Project_terminal_game.py
@@ -11,6 +11,7 @@ def informar_dados_financiamento():
 
 
 class Dados_financiamento:
+
       def __init__(self, recursos_proprios, financiado, juros_balcao, juros_reduzidos, prazo):
             self.recursos_proprios = recursos_proprios
             self.financiado = financiado
@@ -20,32 +21,33 @@ class Dados_financiamento:
             self.prazo = prazo
             self.preco = self.recursos_proprios + self.financiado
 
-# class Encargo(Dados_financiamento):
-#       encargo_count = 0
-#       encargo_restante = 0
-#       def __init__(self, mes, amortizacao):
-#             super().__init__()
-#             self.mes = mes
-#             self.amorticao = amortizacao
-#             Encargo.encargo_count += 1
-#             self.id = Encargo.encargo_count
-#             self.encargo_restante = Dados_financiamento.prazo
-#
-#       def encargo_mensal(self):
-#             juros = (self.saldo * self.juros_reduzidos) # 8189.63
-#             self.saldo += juros
-#             prestacao = self.amorticao + juros #12078.53
-#             valor_seguro = self.saldo * self.seguro
-#             encargo = prestacao + valor_seguro # 12829.19
-#             Encargo.encargo_restante -= 1
-#             print(f'Encargo n. {self.id}    Mês: {self.mes}\n'
-#                   f' Saldo inicial: R$ {self.saldo:6.2f}\n'
-#                   f' Prestação: R$ {prestacao:6.2f}\n'
-#                   f' Juros: R$ {juros:6.2f} taxa: {self.juros_reduzidos}\n'
-#                   f' Seguro: R$ {valor_seguro:6.2f}\n'
-#                   f' Valor do encargo: R$ {round(encargo,2):6.2f}\n'
-#                   f' Prestações pagas: {Encargo.encargo_count}\n'
-#                   f' Prestações restantes: {Encargo.encargo_restante}')
+class Encargo_mensal(Dados_financiamento):
+      encargo_count = 0
+      encargo_restante = 0
+      def __init__(self, mes, amortizacao):
+            super().__init__(recursos_proprios, financiado, juros_balcao, juros_reduzidos, prazo)
+            self.mes = mes
+            self.amortizacao = amortizacao
+            Encargo_mensal.encargo_count += 1
+            self.id = Encargo_mensal.encargo_count
+            #self.encargo_restante = Dados_financiamento.prazo
+
+
+      # def encargo_mensal(self):
+      #       juros = (self.saldo * self.juros_reduzidos) # 8189.63
+      #       self.saldo += juros
+      #       prestacao = self.amorticao + juros #12078.53
+      #       valor_seguro = self.saldo * self.seguro
+      #       encargo = prestacao + valor_seguro # 12829.19
+      #       Encargo.encargo_restante -= 1
+      #       print(f'Encargo n. {self.id}    Mês: {self.mes}\n'
+      #             f' Saldo inicial: R$ {self.saldo:6.2f}\n'
+      #             f' Prestação: R$ {prestacao:6.2f}\n'
+      #             f' Juros: R$ {juros:6.2f} taxa: {self.juros_reduzidos}\n'
+      #             f' Seguro: R$ {valor_seguro:6.2f}\n'
+      #             f' Valor do encargo: R$ {round(encargo,2):6.2f}\n'
+      #             f' Prestações pagas: {Encargo.encargo_count}\n'
+      #             f' Prestações restantes: {Encargo.encargo_restante}')
 
 
 
@@ -53,8 +55,11 @@ class Dados_financiamento:
 recursos_proprios, financiado, juros_balcao, juros_reduzidos, prazo = informar_dados_financiamento()
 financiamento = Dados_financiamento(recursos_proprios, financiado, juros_balcao, juros_reduzidos, prazo)
 
-# encargo_2020_out = Encargo('2020/outubro', 3888.88)
-# print(encargo_2020_out.recursos_proprios)
+encargo_2020_out = Encargo_mensal('2020/outubro', 3888.88)
+#print(dir(financiamento))
+#print(dir(encargo_2020_out))
+print(encargo_2020_out.recursos_proprios)
+print(encargo_2020_out.preco)
 
 
 
