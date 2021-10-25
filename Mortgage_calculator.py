d21d50cdbf8a5061afcd08024d51495373682f61
ElianeSato <eliane.sato.dev@gmail.com>
2021-10-25 08:24:37 -0300

    Adds pyinput module to control input

--- Mortgage_calculator.py
+++ Mortgage_calculator.py
@@ -1,26 +1,30 @@
 #! python3
 # Final project in "CS 101: Introduction to Programming" class: Mortgage calculator
 
+import os
+import pyinputplus as pyip
+
 # calculating monthly_payment: monthly_payment = ((6.5 / 100 / 12) * 200000) / (1 - ((1 + (6.5 / 100 / 12)) ^ (-30 * 12)))
 class Mortgage:
     def __init__(self):
-        self.principal = float(input("Type loan's principal: "))
-        self.interest_rate = float(input("Type yearly interest rate: "))
+        self.principal = pyip.inputFloat("Type loan's principal: ")
+        self.interest_rate = pyip.inputFloat("Type yearly interest rate: ")
         self.interest_rate = self.interest_rate / 100 / 12  # yearly percentage rate divided by 12
 
     def monthly_amount(self):
-        self.yearly_term = int(input("Type yearly term: "))
+        self.yearly_term = pyip.inputInt("Type yearly term: ")
         self.yearly_term *= 12 #number of monthly payments
         payment = ((self.interest_rate * self.principal) / ((1 - ((1 + (self.interest_rate)) ** (-self.yearly_term)))))
-        return f'Monthly amount payment: ${payment:6.2f}'
+        return f'Monthly amount payment for ${self.principal:6.2f} in {self.yearly_term} months: ${payment:6.2f}'
 
     def term(self):
-        self.payment = float(input("Type the monthly amount: "))
+        self.payment = pyip.inputFloat("Type the monthly amount: $")
         term = 0
-        while self.principal > 0:
-            self.principal = self.principal + (self.interest_rate * self.principal) - self.payment
+        balance = self.principal
+        while balance > 0:
+            balance = balance + (self.interest_rate * balance) - self.payment
             term += 1
-        return f'This amount would take {term} months to be paid.'
+        return f"The loan's principal ${self.principal} would take {term} months to be paid."
 
 #print(mortgage.term)
 # testing monthly_amount()
@@ -29,22 +33,24 @@ class Mortgage:
 #testing term()
 #print(mortgage.term()) # output: 360 months
 
+clean_screen = os.system('clear')
+
 mes = 'Welcome to the Mortgage Calculator!'
-print('=' * len(mes))
-print(mes)
-print('=' * len(mes))
-try:
-    mortgage = Mortgage()
-    print("Choose 'monthly amount' or 'loan's term'.")
-    choice = input('Your choice: ')
-    if choice == 'monthly amount':
-        print(mortgage.monthly_amount())
-    elif choice == "loan's term" or choice == "term":
-        print(mortgage.term())
-    else:
-        print('Option is not available.')
-except:
+print('\t'+ ('=' * len(mes)))
+print('\t'+ mes)
+print('\t'+ ('=' * len(mes)))
+
+mortgage = Mortgage()
+print("Choose '1' for monthly amount or '2' for loan's term(years).")
+choice = pyip.inputMenu(choices=['monthly amount', "loan's term"], prompt='Your choice: \n', numbered=True)
+print(choice)
+if choice == 'monthly amount':
+    print(mortgage.monthly_amount())
+elif choice == "loan's term":
+    print(mortgage.term())
+else:
     print('Option is not available.')
+
 
 
 
